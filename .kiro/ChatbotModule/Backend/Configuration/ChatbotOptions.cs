ba8f232170e14c4eafd844658fcfdd40057ce8b4
khanhnguyen2102003 <127201068+khanhnguyen2102003@users.noreply.github.com>
2025-12-03 22:01:55 +0700

    Delete .kiro directory

--- .kiro/ChatbotModule/Backend/Configuration/ChatbotOptions.cs
+++
@@ -1,9 +0,0 @@
-namespace WebAPI.Configuration;
-
-public class ChatbotOptions
-{
-    public int RateLimitRequests { get; set; } = 15;
-    public int RateLimitWindowSeconds { get; set; } = 60;
-    public int RequestTimeoutSeconds { get; set; } = 30;
-    public int RetryMaxAttempts { get; set; } = 3;
-}
